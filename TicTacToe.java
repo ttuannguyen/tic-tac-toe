7b4e677a2fca1e69d8db86497c36227ac60324e7
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-01-03 21:58:27 -0800

    initialize a 2D array for the board and add a  print method to print it

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,32 @@
-
+import java.util.Scanner;
 
 public class TicTacToe {
 
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
+        System.out.println("\nLet's play tic tac toe");
+
+        char[][] board = {
+            { '_', '_', '_' }, 
+            { '_', '_', '_' }, 
+            { '_', '_', '_' }
+          };
+
+        
+        printBoard(board);  
+
+
     }
+
+    public static void printBoard(char[][] board) {
+        System.out.print("\n");  
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++)
+            System.out.print(board[i][j] + " ");
+            System.out.print("\n\n");  
+          }
+      }
     
 }
