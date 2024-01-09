22cd8210f714d3ce51664c2380c09fe355a1edd2
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-01-08 19:37:11 -0800

    implement the logic to check the columns to deterime the result of the game

--- TicTacToe.java
+++ TicTacToe.java
@@ -36,10 +36,12 @@ public class TicTacToe {
 
             if (count == 3) {
                 System.out.println("X wins!");
+                break;
             } else if (count == -3) {
                 System.out.println("O wins");
+                break;
             } else if (i == 8) {
-                System.out.println("No one wins!");
+                System.out.println("It's a tie!");
             }
 
 
@@ -73,7 +75,9 @@ public class TicTacToe {
         if (Math.abs(rows) == 3)
             return rows;
         
-        
+        int columns = checkColumns(board);
+        if (Math.abs(columns) == 3)
+            return columns;
 
         // placeholder
         return -1;
@@ -90,6 +94,22 @@ public class TicTacToe {
                 if (board[i][j] == 'X') {
                     count++;
                 } else if (board[i][j] == 'O') {
+                    count--;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int checkColumns(char[][] board) {
+        int count = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                // By flipping i and j like below, we keep column position static and make row position dynamic as we go through each iteration
+                if (board[j][i] == 'X') {
+                    count++;
+                } else if (board[j][i] == 'O') {
                     count--;
                 }
             }
